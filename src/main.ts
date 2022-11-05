5361476cf45b531e655e5f87a498fc15bc48d808
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-11-06 01:26:31 +0300

    server update

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+	const PORT = 5000;
 	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+	await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 bootstrap();
